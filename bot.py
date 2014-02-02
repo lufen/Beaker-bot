25131cb5a2283a33e0663b2dd5f23b0f4a475b69
= <=>
2014-02-02 16:03:46 +0100

    more stuff  Please enter the commit message for your changes. Lines starting

--- bot.py
+++ bot.py
@@ -22,6 +22,7 @@ class SkypeBot(object):
         time.sleep(20)
         self.replacements = {"cloud": "butt", "Cloud": "Butt", "Butt":"Cloud", "butt": "cloud", "the cloud": "my butt", "The cloud": "My butt"}
         self.tag = "@"
+    
     def MessageStatus(self, msg, status):
         if status == Skype4Py.cmsReceived:
             text = msg.Body
@@ -30,16 +31,17 @@ class SkypeBot(object):
                 newText = self.multiple_replace(self.replacements, text)
                 msg.Chat.SendMessage(newText)
             elif text[0] == self.tag:
-                text = text[1:]
-                command = text.split(" ")[0]
+                text = text[1:] #remove the tag from the text
+                command = text.split(" ")[0] #get the command
                 if command.lower() == "nsfw":
                     url = self.fetch_randNSFW()
                     msg.Chat.SendMessage(url)
                 elif command.lower() == "say":
-                    msg.Chat.SendMessage(text[(len(command) + 1):])
+                    repeat_start = len(command) + 1
+                    msg.Chat.SendMessage(text[repeat_start:])
 
                 elif command.lower() == "help":
-                    text = "Available functions \n Help\tDisplays this help text\nsay\tMake me say a message\nnsfw\tPrint a random NSFW subreddit"
+                    text = "Available functions \nHelp    Displays this help text\nsay    Make me say a message\nnsfw     Print a random NSFW subreddit"
                     msg.Chat.SendMessage(text)
 
 
