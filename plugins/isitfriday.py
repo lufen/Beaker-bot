c3b0b201ae023aaf18171955d63fdd136bf06694
jvlomax <northlomax@gmail.com>
2014-07-24 15:08:32 +0200

    Update isitfriday.py

--- plugins/isitfriday.py
+++ plugins/isitfriday.py
@@ -6,7 +6,7 @@ from datetime import date
 class IsItFriday(Plugin):
     def __init__(self, skype):
         super(IsItFriday, self).__init__(skype)
-        self.falseMessages =["The world is still safe, Russia has not declared war yet", "http://suptg.thisisnotatrueending.com/archive/29138254/images/1388285593271.jpg", "http://www.meh.ro/original/2010_03/meh.ro3771.jpg", "http://d24w6bsrhbeh9d.buttfront.net/photo/arpBmWp_700b_v1.jpg", "http://d24w6bsrhbeh9d.buttfront.net/photo/aXb2VAv_700b.jpg", "http://d24w6bsrhbeh9d.buttfront.net/photo/aLKXd6v_700b.jpg"]
+        
     def message_received(self, msg, status):
         text = msg.Body
         if text[0] == "@":
