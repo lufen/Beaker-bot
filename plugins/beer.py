50f6decc56d6d853f8eae01a188181e0012a9449
lufen <chrihau@gmail.com>
2014-10-02 14:57:45 +0200

    Update to beer plugin

--- plugins/beer.py
+++ plugins/beer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import xml.etree.ElementTree as ET
+import urllib
 
 import requests
 
 from baseclass import Plugin
-import urllib
+
 
 class Beer(Plugin):
     def __init__(self, skype):
@@ -14,15 +15,18 @@ class Beer(Plugin):
 
     def message_received(self, args, status, msg):
         city = " ".join(args).title().decode('utf-8')
-        print urllib.quote(city.encode('utf-8'))
         r = requests.get(
-            'http://beermapping.com/webservice/loccity/7829347ccd56f03274b428b2fca37c77/' + urllib.quote(city.encode('utf-8')))
-        root = ET.fromstring((r.text.encode('utf-8')))
+            'http://beermapping.com/webservice/loccity/7829347ccd56f03274b428b2fca37c77/' + urllib.quote(
+                city.encode('utf-8')))
+        root = ET.fromstring(r.text.encode('utf-8'))
         locs = root.iter('location')
         num = 0
         for loc in locs:
             if loc[6].text != None:
-                out = "Found {0}  at {1}, {2} which is a {3}".format(loc[1].text.encode('utf-8'), loc[6].text.encode('utf-8'), loc[7].text.encode('utf-8'), loc[2].text.encode('utf-8'))
+                out = "Found {0} at {1}, {2} which is a {3}".format(loc[1].text.encode('utf-8'),
+                                                                    loc[6].text.encode('utf-8'),
+                                                                    loc[7].text.encode('utf-8'),
+                                                                    loc[2].text.encode('utf-8'))
                 msg.Chat.SendMessage(out)
                 num += 1
                 if num > 3:
@@ -32,4 +36,4 @@ class Beer(Plugin):
 
     def help(self, msg):
         msg.Chat.SendMessage(
-            "Usage: @beer place\nWill tell you about great places to find beer in the US")
+            "Usage: @beer city\nWill tell you about great places to find beer")
